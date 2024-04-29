c6dbb9e61c1989960a5173f0e833d80802c48161
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-04-28 23:34:53 -0700

    Add starter code vid converter

---
+++ rag/file_conversion_router/conversion/video_converter.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from rag.file_conversion_router.conversion.base_converter import BaseConverter
+
+
+class PdfConverter(BaseConverter):
+    def __init__(self):
+        super().__init__()
+
+    # Override
+    def _to_markdown(self, input_path: Path, output_path: Path) -> None:
+        """Perform PDF to Markdown conversion using Nougat with the detected hardware configuration."""
+        raise NotImplementedError("This method is not implemented yet.")

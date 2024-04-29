790460b57b6a085c944798506aa344af0d65284a
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-04-29 14:14:47 -0700

    Refract code

--- tests/rag/test_file_conversion_router/test_services/test_task_manager.py
+++ tests/rag/test_file_conversion_router/test_services/test_task_manager.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 import pytest
 
@@ -8,15 +9,16 @@ from tests.rag.utils import compare_files
 
 
 @pytest.mark.parametrize(
-    "input_path, expected_output_path",
-    load_test_cases_config("unit_tests", "pdf_to_md"),
+    "input_path, expected_output_paths",
+    load_test_cases_config("unit_tests", "pdf_converter"),
 )
-def test_pdf_to_md_conversion(input_path: str, expected_output_path: str, tmp_path, pdf_to_md_converter):
-    input_path, expected_path = Path(input_path), Path(expected_output_path)
-    output_path = tmp_path / input_path.with_suffix(".mmd").name
+def test_pdf_converter_on_task_manager(input_path: str, expected_output_paths: List[str], tmp_path, pdf_converter):
+    input_path = Path(input_path)
+    expected_paths = [Path(expected_output_path) for expected_output_path in expected_output_paths]
+    output_folder = tmp_path / input_path.stem
 
     # Schedule the conversion using the asynchronous scheduler
-    future = schedule_conversion(pdf_to_md_converter.convert, input_path, output_path)
+    future = schedule_conversion(pdf_converter.convert, input_path, output_folder)
 
     # Wait for the task to complete and check for exceptions
     try:
@@ -25,5 +27,9 @@ def test_pdf_to_md_conversion(input_path: str, expected_output_path: str, tmp_pa
         pytest.fail(f"Conversion failed with an exception: {e}")
 
     # Now that the task is complete, check the file against the expected output
-    matches_expectation = compare_files(expected_path, output_path)
-    assert matches_expectation, f"File conversion for {input_path} did not meet expectations."
+    for idx, suffix in enumerate([".md", ".md.pkl", ".md_tree.txt"]):
+        output_file_path = output_folder / f"{input_path.stem}{suffix}"
+        assert output_file_path.exists(), f"File {output_file_path} does not exist."
+        assert compare_files(
+            expected_paths[idx], output_file_path
+        ), f"File conversion for {input_path} did not meet expectations."

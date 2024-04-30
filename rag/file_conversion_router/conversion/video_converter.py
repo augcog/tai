229e01d354ee8b5aa805461e52c0e6154319b5b7
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-04-30 16:23:41 -0700

    Add TODOs for Wayne

--- rag/file_conversion_router/conversion/video_converter.py
+++ rag/file_conversion_router/conversion/video_converter.py
@@ -10,4 +10,5 @@ class VideoConverter(BaseConverter):
     # Override
     def _to_markdown(self, input_path: Path, output_path: Path) -> None:
         """Perform Video to Markdown conversion."""
+        # TODO: Implement Video to Markdown conversion by integrating our existing video processing pipeline.
         raise NotImplementedError("Video to Markdown conversion is not supported.")
--- tests/test_rag/conftest.py
+++ tests/test_rag/conftest.py
@@ -109,7 +109,6 @@ def helper_unit_test_on_converter(input_path: str, expected_output_paths: List[s
     output_folder = tmp_path / input_path.stem
     converter.convert(input_path, output_folder)
 
-    # TODO: revise this test after MarkdownParser fixes the output file path name issue
     for idx, suffix in enumerate([".md", ".md.pkl", ".md.tree.txt"]):
         output_file_path = output_folder / f"{input_path.stem}{suffix}"
         assert output_file_path.exists(), f"File {output_file_path} does not exist."

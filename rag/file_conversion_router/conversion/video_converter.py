e0f3fb3f0a0a25f1e722a12197a2cee83a433dd2
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-04-29 22:25:41 -0700

    Rename convert class name

--- rag/file_conversion_router/conversion/video_converter.py
+++ rag/file_conversion_router/conversion/video_converter.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from rag.file_conversion_router.conversion.base_converter import BaseConverter
 
 
-class PdfConverter(BaseConverter):
+class VideoConverter(BaseConverter):
     def __init__(self):
         super().__init__()
 
     # Override
     def _to_markdown(self, input_path: Path, output_path: Path) -> None:
-        """Perform PDF to Markdown conversion using Nougat with the detected hardware configuration."""
-        raise NotImplementedError("This method is not implemented yet.")
+        """Perform Video to Markdown conversion."""
+        raise NotImplementedError("Video to Markdown conversion is not supported.")

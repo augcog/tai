e108ef9b3716d48e8f19801b477480692ad156b6
Catrunaround <129796960+Catrunaround@users.noreply.github.com>
2024-07-12 01:33:51 -0700

    fix compare question

--- rag/file_conversion_router/conversion/pdf_converter.py
+++ rag/file_conversion_router/conversion/pdf_converter.py
@@ -36,26 +36,19 @@ class PdfConverter(BaseConverter):
 
     # Override
     def _to_markdown(self, input_path: Path, output_path: Path) -> Path:
-        input_pdf = str(input_path)
-        folder_name =  str(output_path.parent)
-        output_pdf = process_pdf(input_pdf)
-
         # """Perform PDF to Markdown conversion using Nougat with the detected hardware configuration."""
-        command = [
-            "nougat",
-            str(input_path),
-            # nougat requires the argument output path to be a directory, not file, so we need to handle it here
-            "-o",
-            str(output_path.parent),
-            "--no-skipping",
-            "--model",
-            self.model_tag,
-            "--batchsize",
-            str(self.batch_size),
-        ]
-
-        mmd_file_path = generate_mmd_file_path(folder_name)
-        pdf_to_md(output_pdf, folder_name, mmd_file_path)
+        # command = [
+        #     "nougat",
+        #     str(input_path),
+        #     # nougat requires the argument output path to be a directory, not file, so we need to handle it here
+        #     "-o",
+        #     str(output_path.parent),
+        #     "--no-skipping",
+        #     "--model",
+        #     self.model_tag,
+        #     "--batchsize",
+        #     str(self.batch_size),
+        # ]
         # try:
         #     result = subprocess.run(command, check=False, capture_output=True, text=True)
         #     self._logger.info(f"Output: {result.stdout}")
@@ -63,7 +56,6 @@ class PdfConverter(BaseConverter):
         #     if result.returncode != 0:
         #         self._logger.error(f"Command exited with a non-zero status: {result.returncode}")
         #     # Now change the file name of generated mmd file to align with the expected md file path from base converter
-        output_path = Path(mmd_file_path)
         output_mmd_path = output_path.with_suffix(".mmd")
         # Rename it to `md` file
         target = output_path.with_suffix(".md")

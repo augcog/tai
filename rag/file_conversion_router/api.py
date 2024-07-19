343234dd4ba46b21e0be02c229ebb21804ae089b
Catrunaround <129796960+Catrunaround@users.noreply.github.com>
2024-07-19 16:56:26 -0700

    delet api test func

--- rag/file_conversion_router/api.py
+++ rag/file_conversion_router/api.py
@@ -16,5 +16,3 @@ def convert_directory(input_dir: Union[str, Path], output_dir: Union[str, Path])
     """
     process_folder(input_dir, output_dir)
 
-if __name__ == "__main__" :
-    convert_directory("tests\\test_rag\\data\\integrated_tests\\input_folder2_nested_folder_pdf+md", "tests\\test_rag\\data\\integrated_tests\\output_test")

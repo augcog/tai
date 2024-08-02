c2b51ae1da9af2d11788fa194ec9b20f09bf40cc
Catrunaround <129796960+Catrunaround@users.noreply.github.com>
2024-08-02 01:16:04 -0700

    use new pkl instead

--- rag/file_conversion_router/api.py
+++ rag/file_conversion_router/api.py
@@ -17,4 +17,8 @@ def convert_directory(input_dir: Union[str, Path], output_dir: Union[str, Path])
     process_folder(input_dir, output_dir)
 
 if __name__ == "__main__": 
-    convert_directory("tests\\test_rag\data\integrated_tests\input_folder2_nested_folder_pdf+md\mds", "tests\\test_rag\data\integrated_tests\expected_output_folder2_nested_folder_pdf+md\mds")
+    # convert_directory("tests\\test_rag\\data\\integrated_tests\\input_folder1_plain_folder_3_pdfs", "tests\\test_rag\\data\\integrated_tests\\expected_output_folder1_plain_folder_3_pdfs")
+    # convert_directory("tests\\test_rag\\data\\integrated_tests\\input_folder2_nested_folder_pdf+md", "tests\\test_rag\\data\\integrated_tests\\expected_output_folder2_nested_folder_pdf+md")
+    # convert_directory("tests\\test_rag\\data\\unit_tests\\md\\input", "tests\\test_rag\\data\\unit_tests\\md\\expected_output")
+    # convert_directory("tests\\test_rag\\data\\unit_tests\\pdf\\input", "tests\\test_rag\\data\\unit_tests\\pdf\\expected_output")
+    # convert_directory("output_tmp\input", "output_tmp\expected_output")

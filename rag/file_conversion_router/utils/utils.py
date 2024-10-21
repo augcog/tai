a4203d118b470e12ccef472eed51d34661639cb2
HaichaoLihc <lhc20050616@berkeley.edu>
2024-10-20 21:24:35 -0700

    add content check function for conversion

--- rag/file_conversion_router/utils/utils.py
+++ rag/file_conversion_router/utils/utils.py
@@ -2,7 +2,7 @@
 from hashlib import sha256
 from pathlib import Path
 from typing import Union
-
+import requests
 
 def ensure_path(path: Union[str, Path]) -> Path:
     return Path(path) if not isinstance(path, Path) else path
@@ -15,3 +15,12 @@ def calculate_hash(input_path: Path) -> str:
         for byte_block in iter(lambda: file.read(4096), b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
+
+
+def check_url(url: str) -> int:
+    try:
+        response = requests.head(url, allow_redirects=True, timeout=10)
+        return response.status_code
+    except requests.exceptions.RequestException as e:
+        print(f"Error accessing {url}: {e}")
+        return None

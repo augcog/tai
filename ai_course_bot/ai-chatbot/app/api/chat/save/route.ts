96f9a3a1372b007b61694c2477aa4c3f54ed1ef8
Steve Gao <steve_gao@berkeley.edu>
2025-01-16 18:29:40 -0800

    change to mongodb

--- ai_course_bot/ai-chatbot/app/api/chat/save/route.ts
+++ ai_course_bot/ai-chatbot/app/api/chat/save/route.ts
@@ -1,34 +1,11 @@
 import { nanoid } from '@/tai/lib/utils'
-import { kv } from '@vercel/kv'
 import { auth } from '@/tai/utils/auth'
+import { saveChat } from '@/tai/utils/actions'
 
 export async function POST(req: Request) {
   const json = await req.json()
   const userId: string = (await auth())?.user.id ?? nanoid()
   const title = json.messages[0].content.substring(0, 100)
-  saveChat(title, json.messages, userId, json.id)
+  await saveChat(title, json.messages, userId, json.id)
   return new Response()
-}
-
-async function saveChat(
-  title: string,
-  messages: any,
-  userId: string,
-  id: string
-) {
-  const createdAt = Date.now()
-  const path = `/chat/${id}`
-  const payload = {
-    id,
-    title,
-    userId,
-    createdAt,
-    path,
-    messages
-  }
-  await kv.hmset(`chat:${id}`, payload)
-  await kv.zadd(`user:chat:${userId}`, {
-    score: createdAt,
-    member: `chat:${id}`
-  })
 }

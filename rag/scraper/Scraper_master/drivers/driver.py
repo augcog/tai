65151170f0ce4e90c8f1c602312cf0d6a1ca57b6
HaichaoLihc <lhc20050616@berkeley.edu>
2025-01-30 19:55:05 -0800

    updated scraper

---
+++ rag/scraper/Scraper_master/drivers/driver.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+class Driver(ABC):
+    @abstractmethod
+    def download_raw(self, filename, url):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+@dataclass
+class Resp():
+    html_content: str
+    is_html: bool
+    true_url: str

9d26f90b1fc0dd46a59d20a3ee247daf9063ebf8
HaichaoLihc <lhc20050616@berkeley.edu>
2025-03-17 11:43:19 -0700

    add basic conversion cache

--- rag/file_conversion_router/services/directory_service.py
+++ rag/file_conversion_router/services/directory_service.py
@@ -6,7 +6,7 @@ from concurrent.futures import as_completed
 from pathlib import Path
 from typing import Dict, Type, Union
 
-from rag.file_conversion_router.conversion.base_converter import BaseConverter, ConversionCache
+from rag.file_conversion_router.conversion.base_converter import BaseConverter
 from rag.file_conversion_router.conversion.md_converter import MarkdownConverter
 from rag.file_conversion_router.conversion.pdf_converter import PdfConverter
 from rag.file_conversion_router.conversion.rst_converter import RstConverter
@@ -15,10 +15,8 @@ from rag.file_conversion_router.conversion.ed_converter import EdConverter
 from rag.file_conversion_router.conversion.html_converter import HtmlConverter
 from rag.file_conversion_router.services.task_manager import schedule_conversion
 from rag.file_conversion_router.utils.logger import content_logger, set_log_file_path
-from rag.file_conversion_router.utils.utils import calculate_hash
-from rag.file_conversion_router.utils.persistent_cache import *
-from rag.file_conversion_router.utils.utils import load_conversion_version, is_empty_md
-
+from rag.file_conversion_router.utils.conversion_cache import ConversionCache
+from rag.file_conversion_router.utils.utils import load_conversion_version
 
 ConverterMapping = Dict[str, Type[BaseConverter]]
 
@@ -35,7 +33,7 @@ converter_mapping: ConverterMapping = {
 
 
 def process_folder(input_dir: Union[str, Path], output_dir: Union[str, Path],
-                   log_dir: Union[str, Path] = None, cache_dir: Union[str, Path] = None) -> None:
+                   log_dir: Union[str, Path] = None, cache_path: Union[str, Path] = None) -> None:
     """Walk through the input directory and schedule conversion tasks for specified file types.
 
     Args:
@@ -50,16 +48,8 @@ def process_folder(input_dir: Union[str, Path], output_dir: Union[str, Path],
     output_dir = Path(output_dir)
     input_dir = Path(input_dir)
 
-    cache_file_name = "persistent_cache.pkl"
-    persistent_cache = {}
     if log_dir:
         set_log_file_path(content_logger, log_dir)
-    if cache_dir:
-        cache_path = Path(cache_dir) / cache_file_name
-        persistent_cache = load_persistent_cache(cache_path)
-
-    version_file_path = Path(__file__).parent.parent / "conversion_version.txt"
-    conversion_version = load_conversion_version(version_file_path)
 
     if not input_dir.is_dir():
         raise ValueError(f"Provided input path {input_dir} is not a directory.")
@@ -74,21 +64,14 @@ def process_folder(input_dir: Union[str, Path], output_dir: Union[str, Path],
     valid_extensions = tuple(converter_mapping.keys())
     futures = []
 
+    if cache_path:
+        ConversionCache.set_cache_path(cache_path)
+    else:
+        ConversionCache.set_cache_path(None)
+
     # Iterate over all files with specified extensions
     for input_file_path in input_dir.rglob("*"):
         if input_file_path.suffix in valid_extensions and input_file_path.is_file():
-            if cache_dir:
-                file_hash = calculate_hash(input_file_path)
-                if is_empty_md(file_hash):
-                    content_logger.error(f"conversion skipped: found empty markdown file {input_file_path}")
-                    continue
-                cached_result = persistent_cache.get(file_hash, None)
-                if cached_result and cached_result == conversion_version:
-                    content_logger.info(f"Using persistent cached result version {conversion_version} for {input_file_path}")
-                    continue
-                else:
-                    persistent_cache[file_hash] = conversion_version
-
             # Construct the output subdirectory and file path
             output_subdir = output_dir / input_file_path.relative_to(input_dir).parent
             output_subdir.mkdir(parents=True, exist_ok=True)
@@ -114,10 +97,6 @@ def process_folder(input_dir: Union[str, Path], output_dir: Union[str, Path],
         except Exception as e:
             logging.error(f"Conversion failed: {e}")
 
-    if cache_dir:
-        cache_dir = Path(cache_dir)
-        os.makedirs(cache_dir, exist_ok=True)
-        save_persistent_cache(persistent_cache, cache_dir / cache_file_name)
     content_logger.info(f"Completed content checking for directory: {input_dir}")
     logging.info(f"Completed processing for directory: {input_dir}")
     logging.info(f"Saved conversion time [{ConversionCache.calc_total_savings()} seconds] by using cached results.")

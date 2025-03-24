51f9224cf419493d577235427b245b3d0108d074
terrianne-zhang <terriannezhang@berkeley.edu>
2025-03-24 13:15:08 -0700

    working evaluation pipeline

--- rag/tests/evaluation.py
+++ rag/tests/evaluation.py
@@ -3,16 +3,18 @@ import openai
 from dotenv import load_dotenv
 import csv
 import os
+import pickle
+import time
 
 load_dotenv()
 
 # QUESTIONS:
-questions = ["How can I rapidly become proficient in using the MoveIt Motion Planning Framework, specifically for the Franka Emika Panda robot, through the provided tutorials, and are there alternative robots that are already compatible with MoveIt? Please list them and provide me with a resource for further exploration. Additionally, if my custom robot is not yet supported by MoveIt, how can I integrate it into the framework using the tutorial section ""Integration with a New Robot""? Provide me with a step-by-step guide and necessary resources.",
-             "How can I utilize the MoveGroup interface, available in both C++ and Python, to efficiently interact with MoveIt and access its various features through scripting, as described in the 'MoveGroup - ROS Wrappers in C++ and Python' section of this document? Please provide a step-by-step guide, including any necessary prerequisites and examples of how to use the interface to manipulate the robot's movements and configurations. Additionally, clarify any potential limitations or trade-offs of relying solely on the MoveGroup interface, and offer alternatives or complementary approaches for more advanced MoveIt applications.",
-             "How can developers utilize MoveIt's C++ API to build more complex applications with significantly faster performance, and what are the potential benefits of bypassing the ROS Service/Action layers in this process? Please provide specific examples and use cases to illustrate the advantages of directly accessing MoveIt's C++ API.",
-             "How can I implement time parameterization for motion planning in MoveIt! using the Time Parameterization tutorial provided in the documentation, and what are the benefits of using this technique in my robot's motion planning process? Additionally, can you provide examples of how to apply this technique in specific motion planning scenarios, and how to integrate it with other MoveIt! features such as the motion planning pipeline and planning scene? Finally, what are the system requirements for using time parameterization in MoveIt!, and how can I ensure optimal performance when implementing this technique in my robot's motion planning system?",
-             "How can I integrate a new robot with MoveIt, and what tutorials are available in the 'Integration with a New Robot' section to help with this process? Provide a detailed explanation and list all the necessary steps involved in integrating a new robot with MoveIt, as well as any prerequisites or requirements that should be met before beginning the integration process. Additionally, describe how these tutorials can be accessed and navigated within the 'Integration with a New Robot' section."
-            ]
+with open('/home/bot/bot/terrianne-eval/tai/rag/tests/zephyr_400_questions.pkl', 'rb') as file:
+    data = pickle.load(file)
+
+questions_all = [data[i][1] for i in range(len(data))]
+questions = questions_all[:100]
+
 
 # CONFIGS
 get_chunks_url = "http://0.0.0.0:8000/api/chat/top_k_docs"
@@ -102,7 +104,7 @@ Score 5: The response completely uses the main ideas of the reference context to
 """
 
 # evaluates overall factualness of response to a question and helpfulness to student
-# see if it is giving good answer to the question -- still provide chunks for reference. want to show as review of TAI, score based on helpfulness 
+# see if it is giving good answer to the question -- still provide chunks for reference. want to show as review of TAI, score based on helpfulness
 EVALUATE_RESPONSE = """###Task Description:
 You are evaluating the reference context provided to a Retrieval-Augmented Generation-based LLM to answer a question related to the Berkeley course {course} asked by a student. You are responsible for scoring the response following a score rubric for the response.
 1. Write detailed feedback that assesses this quality of the response strictly based on the given score rubric, not evaluating in general.
@@ -165,7 +167,7 @@ def query_tai_llm(messages, model="gpt-4-turbo", temperature=0.7):
 
 question_context_answer_feedback = []
 
-for question in questions:
+for i, question in enumerate(questions):
     params = {
         "message": question,
         "k": 5,
@@ -206,9 +208,15 @@ for question in questions:
         'temperature': 0
     }
 
-    response1 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers1, json=data1)
-    result1 = response1.json()
-    feedback1 = result1['choices'][0]['message']['content']
+    for _ in range(3):
+        try:
+            response1 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers1, json=data1)
+            result1 = response1.json()
+            break
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error, retrying in 5 seconds")
+            time.sleep(5)
+    feedback1 = result1['choices'][0]['message']['content'] if result1 else "No response"
 
     prompt2 = EVALUATE_CHUNK_QUALITY.format(
         course=COURSE,
@@ -228,9 +236,15 @@ for question in questions:
         'temperature': 0
     }
 
-    response2 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers2, json=data2)
-    result2 = response2.json()
-    feedback2 = result2['choices'][0]['message']['content']
+    for _ in range(3):
+        try:
+            response2 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers2, json=data2)
+            result2 = response2.json()
+            break
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error, retrying in 5 seconds")
+            time.sleep(5)
+    feedback2 = result2['choices'][0]['message']['content'] if result2 else "No response"
 
     prompt3 = EVALUATE_MODEL.format(
         course=COURSE,
@@ -250,9 +264,15 @@ for question in questions:
         'temperature': 0
     }
 
-    response3 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers3, json=data3)
-    result3 = response3.json()
-    feedback3 = result3['choices'][0]['message']['content']
+    for _ in range(3):
+        try:
+            response3 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers3, json=data3)
+            result3 = response3.json()
+            break
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error, retrying in 5 seconds")
+            time.sleep(5)
+    feedback3 = result3['choices'][0]['message']['content'] if result3 else "No response"
 
     prompt4 = EVALUATE_RESPONSE.format(
         course=COURSE,
@@ -272,11 +292,18 @@ for question in questions:
         'temperature': 0
     }
 
-    response4 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers4, json=data4)
-    result4 = response4.json()
-    feedback4 = result4['choices'][0]['message']['content']
+    for _ in range(3):
+        try:
+            response4 = requests.post('https://api.openai.com/v1/chat/completions', headers=headers4, json=data4)
+            result4 = response4.json()
+            break
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error, retrying in 5 seconds")
+            time.sleep(5)
+    feedback4 = result4['choices'][0]['message']['content'] if result4 else "No response"
 
     question_context_answer_feedback.append([question, context, answer, used_chunks, distances, feedback1, feedback2, feedback3, feedback4])
+    print("Done evaluating question: " + str(i))
 
 output_file = "question_context_feedback.csv"
 
@@ -292,58 +319,3 @@ with open(output_file, mode='w', newline='', encoding='utf-8') as file:
     writer.writerows(question_context_answer_feedback)
 
 print(f"Data written to {output_file}")
-
-
-# analyzing results afterward for response 1-- 2 step retrival, 1. ranking, 2. threshold 
-# - if documents are irrelavent
-# - no documents over similarity threshold -- threshold is too high or no supported documents
-# need to classify into these two items 
-
-# response 2: 
-# - irrelevant documents in context (yes / no) based on score
-
-#+++++++++++++++++++++++++++++++++
-
-#another round of conversation
-# need to return ranked version of all documents from route (including ones below the threshold) to ask LLM to evaluate if they are relevant or not
-
-# is the prompt given to llama3 good or not -- see if gpt4 can evaluate 
-# -- too long prompt -- attention not good
-# -- too short prompt -- not enough information to describe situation
-
-
-
-# dec 5th
-# give example ? of rating per each level of prompt
-# take larger k
-# add cosine similarity of each chunk 
-# one score per chunk 
-# get top 5 k, then evaluate if this is better or worse than top 3 --> add this into the prompt 
-
-# just parse the score out (quantitative answer)
-
-# in the gpt response, explain which one of these is the problem 
-# several options: improve on retrieval accuracy, clean up the chunk, etc. 
-# give improvement advice -- for the second two 
-
-# is the chunk bad to read and is the information clear (new prompt)
-
-# merge prompt one and two, want this and current third prompt to be advice for how to improve retrieval 
-# current 3rd prompt -- does model have general background to connect the dots between the chunks (add this in the advice output)
-# current 4th prompt should be how good the response would be for a student
-
-# compare trends of our scores to other benchmarks / leaderboards
-
-# structured api call for gpt to choose from list of advice 
-
-# milestone: auto-pipeline, take in ed forum file. 
-
-
-#1. retrieval accuracy used to get the best retrieval method (embedding model, reranking model, retrieval algorithms)
-#2. about picking the best LLM for TAI (ex. llama3, deepseek, changing the prompt / giving more exmaples for q and a)
-#3 not used to make adjustments on the method and overall score we want to show to peope who care about performance
-
-# fix prompts, share results, read papers
-
-# how to prepare a good dataset for pipeline for any classes --> ed pipeline, automated question generation pipeline, real test cases 
-# read code for question_generator.py

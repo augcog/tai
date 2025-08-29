a3a7ca1f4fc9737895dc650405e4e4e0176af4c6
Benzhang2004 <zhangjialin04@sina.com>
2025-08-29 15:48:51 -0700

    Naming

--- ai_chatbot_backend/app/services/rag_retriever.py
+++ ai_chatbot_backend/app/services/rag_retriever.py
@@ -17,7 +17,7 @@ from app.dependencies.model import get_embedding_engine
 embedding_model = get_embedding_engine()
 # Environment Variables
 EMBEDDING_PICKLE_PATH = Path("/home/bot/localgpt/tai/ai_chatbot_backend/app/embedding/")
-DB_URI_RO = f"file:{quote('/home/bot/localgpt/tai/ai_chatbot_backend/courses/content.db')}?mode=ro&cache=shared"
+DB_URI_RO = f"file:{quote('/home/bot/localgpt/tai/ai_chatbot_backend/db/metadata.db')}?mode=ro&cache=shared"
 _local = threading.local()
 # SQLDB: whether to use SQL database or Pickle for retrieval.
 SQLDB = True

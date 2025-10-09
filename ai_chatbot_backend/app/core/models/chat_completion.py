513f2c79bbbb9e79ee788d495dc4b872098217b3
= <=>
2025-10-08 17:27:37 -0700

    wip: merge practice completion with completion

--- ai_chatbot_backend/app/core/models/chat_completion.py
+++ ai_chatbot_backend/app/core/models/chat_completion.py
@@ -81,6 +81,21 @@ class CompletionParams(BaseModel):
     audio_response: Optional[bool] = False
     sid: Optional[str] = None  # chat_history_sid from frontend
 
+class CompletionParamsV2(BaseModel):
+    course_code: str
+    audio: VoiceMessage = None
+    messages: List[Message]
+    temperature: float  # TODO: Not in use. Remove?
+    stream: bool
+    chat_type: str = "general"  # e.g., "general", "file" "practice"
+    user_focus: Optional[UserFocus] = None
+    rag: Optional[bool] = True
+    audio_response: Optional[bool] = False
+    sid: Optional[str] = None  # chat_history_sid from frontend
+    answer_content: Optional[str] = None
+    problem_id: Optional[str] = None
+    file_path: Optional[str] = None
+
 class TextCompletionParams(BaseModel):
     course: str
     messages: List[Message]
